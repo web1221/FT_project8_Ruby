4c986b68bbfa2fc7af2da43896ee2e2f3beb704f
Angela Weber <angelaweber95@hotmail.com>
2019-12-20 14:33:09 -0800

    add app.rb and routes to app.rb

---
+++ app.rb
@@ -0,0 +1,15 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
