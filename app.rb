b86476c0c602ca979883b7958919e855fec7d27c
Angela Weber <angelaweber95@hotmail.com>
2019-12-20 14:39:27 -0800

    add post to app.rb. make changes to view files

--- app.rb
+++ app.rb
@@ -13,3 +13,13 @@ get('/words') do
   @words = Word.all
   erb(:words)
 end
+post('/words') do
+  word = Word.new(params[:word_input])
+  word.save
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
