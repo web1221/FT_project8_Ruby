89511977d6fdec8bde703e3b7c1bcae2bb611a7f
Angela Weber <angelaweber95@hotmail.com>
2019-12-20 14:42:29 -0800

    add test for clear word clear method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -23,8 +23,17 @@ describe('#Word') do
       word1.save()
       word2 = Word.new("template", nil)
       word2.save()
-      # binding.pry
       expect(Word.all).to(eq([word1, word2]))
     end
   end
+
+  describe('.clear') do
+    it("clears all albums") do
+      word1 = Word.new("universe", nil)
+      word1.save()
+      word2 = Word.new("template", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
 end
