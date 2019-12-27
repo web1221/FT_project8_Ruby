66c1bdac72f0320e838a86f8cdd4f736b8243796
Angela Weber <angelaweber95@hotmail.com>
2019-12-26 16:39:00 -0800

    add delete funtionality to deleting a definition.

--- app.rb
+++ app.rb
@@ -45,21 +45,21 @@ end
 
 post('/words/:id/definition') do
   @word = Word.find(params[:id].to_i())
-  definition = Definitions.new(params[:definition_input], @word.id, nil)
-  definition.save()
+  @definition = Definitions.new(params[:definition_input], @word.id, nil)
+  @definition.save()
   erb(:word)
 end
 
 patch('words/:id/definition/:definition_id') do
   @word = Word.find(params[:id].to_i())
   definition = Definitions.find(params[:definition_id].to_i())
-  definition.update(params[:definition], @word.id)
+  definition.update(params[:definition_input], @word.id)
   erb(:word)
 end
 
 delete('/words/:id/definition/:definition_id') do
-  @definition = Definitions.find(params[:definition_id].to_i())
-  @definition.delete
+  definition = Definitions.find(params[:definition_id].to_i())
+  definition.delete
   @word = Word.find(params[:id].to_i())
   erb(:word)
 end
