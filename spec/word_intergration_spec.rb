450019cbe664b3d07742473dc5c1e80e2d919f4c
Angela Weber <angelaweber95@hotmail.com>
2020-01-12 17:18:12 -0800

    add and pass integration test to update a word

--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -15,13 +15,23 @@ describe('create an word path', {:type => :feature}) do
     expect(page).to have_content("People")
   end
   it('creates a definition for a word and then goes to the words page') do
-    word = Word.new("People", nil)
-    word.save
-    id = word.id
+    test_word = Word.new("People", nil)
+    test_word.save
+    id = test_word.id
     visit "/words/#{id}"
     fill_in('definition_input', :with => 'human beings in general or considered collectively.')
     click_on('Add a definition')
-    save_and_open_page
     expect(page).to have_content("Human beings in general or considered collectively.")
   end
-end
+  it 'allows a user to change the word' do
+    test_word = Word.new('Charging', nil)
+    test_word.save
+    visit '/'
+    click_link('Charging')
+    click_link('Edit Word')
+    save_and_open_page
+    fill_in('new_word', :with => 'Time')
+    click_button('Update')
+    expect(page).to have_content('Time')
+    end
+  end
