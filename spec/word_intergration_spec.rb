595c79c38f865efbd6cca5fe1498177718f7270f
Angela Weber <angelaweber95@hotmail.com>
2020-01-12 17:10:36 -0800

    add and pass integration test for adding a definition

--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -1,15 +1,27 @@
 require('capybara/rspec')
 require('./app')
+require ('word')
+require ('definition')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('create an word path', {:type => :feature}) do
-  it('creates an word and then goes to the word page') do
+  it('creates an word and then goes to the with all words') do
     visit('/words')
     click_on('Add a word!')
     fill_in('word_input', :with => 'People')
     fill_in('definition_input', :with => 'human beings in general or considered collectively.')
     click_on('Go!')
     expect(page).to have_content("People")
+  end
+  it('creates a definition for a word and then goes to the words page') do
+    word = Word.new("People", nil)
+    word.save
+    id = word.id
+    visit "/words/#{id}"
+    fill_in('definition_input', :with => 'human beings in general or considered collectively.')
+    click_on('Add a definition')
+    save_and_open_page
+    expect(page).to have_content("Human beings in general or considered collectively.")
   end
 end
